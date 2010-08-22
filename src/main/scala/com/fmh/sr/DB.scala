1ae523d784b6d0ee5cdaf19bf4560c8077f823bf
Roman Naumann <roman_naumann@fastmail.fm>
2010-08-22 04:38:07 +0200

    sid

--- src/main/scala/com/fmh/sr/DB.scala
+++ src/main/scala/com/fmh/sr/DB.scala
@@ -28,45 +28,50 @@ import se.scalablesolutions.akka.stm.Transaction.Global._
 import Actor._
 import ScalaConfig._
 
-class DB extends Actor {
+class DB(dbName: String) extends Actor {
   self.lifeCycle = Some(LifeCycle(Permanent))
 
-  val DB_NAME = "sr.nodes"
-  private var db = atomic { MongoStorage.getMap(DB_NAME) }
+  private var db = atomic { MongoStorage.getMap(dbName) }
+
   log.info("MongoDB started...")
 
   override def postRestart(reason: Throwable) = {
-    db = atomic { MongoStorage.getMap(DB_NAME) }
+    db = atomic { MongoStorage.getMap(dbName) }
   }
 
   def receive = {
-    case DB.Put(k,v) => atomic {
-      self reply db.put(k,v)
-    }
-    case DB.Get(k) => self reply atomic {
-      db.get(k) match {
-        case Some(v) => self reply v
-        case _ => ;
-      }
-    }
+    case DB.Clear => self reply_? atomic { db.clear }
+    case DB.Put(k,v) => self reply_? atomic { db.put(k,v) }
+    case DB.Get(k) => self reply atomic { db.get(k) }
   }
 }
 
 object DB {
+  def apply(dbName: String): ActorRef = actorOf(new DB(dbName)).start
+
+  def create(dbName: String): ActorRef = {
+    val storage = apply(dbName)
+    storage !! Clear match {
+      case (Some(())) => return storage
+      case res => throw new RuntimeException("could not empty mongo storage, res: "+res)
+    }
+  }
+
   case class Put(k:AnyRef, v:AnyRef)
   case class Get(k:AnyRef)
+  case class Clear
 }
 
 object DBTest {
   def apply() {
-    val s = actorOf[DB].start
-    s !! DB.Put("1","roman")
+    val s = DB("sr.nodes")
+    s ! DB.Put("1","roman")
     s !! DB.Put("2","hendrik")
     s !! DB.Put("1","fmh")
-    println(s !! DB.Get("1"))
+    println(s !! DB.Get("2"))
+    s !! DB.Clear
+    println(s !! DB.Get("2"))
 
-    /*val storage = MongoStorage.newMap
-    atomic {storage.put("1","roman")}
-    atomic {println(storage.get("1"))}*/
+    System exit 0
   }
 }
